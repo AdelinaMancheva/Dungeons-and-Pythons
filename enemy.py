f82b09faf78d05f20d9926e24b3dca1386bd0fd4
mimirerelala <mirela.napetova@gmail.com>
2015-12-14 20:47:40 +0200

    enemy.py

---
+++ enemy.py
@@ -0,0 +1,31 @@
+class Enemy:
+    def __init__(self, **kwargs):
+        for key in kwargs:
+            self.key = kwargs[key]
+        pass
+
+    def is_alive():
+        pass
+
+    def can_cast():
+        pass
+
+    def get_health():
+        pass
+
+    def get_mana():
+        pass
+
+    def take_healing():
+        pass
+
+    def take_mana():
+        pass
+
+    def attack(self):
+        pass
+
+    def take_damage(self):
+        pass
+
+

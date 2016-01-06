b00425f61dd1a3bd1271d62e0e4dac6ac23633e4
AdelinaMancheva <adelina.mancheva@gmail.com>
2016-01-06 13:53:12 +0200

    small fixes

--- hero.py
+++ hero.py
@@ -3,13 +3,15 @@ from weapon import Weapon
 
 class Hero:
 
-    def __init__(self, name, title, health, start_health, mana, mana_rate):
+    def __init__(self, name, title, health, start_health, mana, mana_rate, curr_weapon, curr_spell):
         self.__name = name
         self.__health = health
         self.__start_health = start_health
         self.__title = title
         self.__mana = mana
         self.__mana_rate = mana_rate
+        self.__curr_weapon = curr_weapon
+        self.__cur_spell = curr_spell
 
     def __str__(self):
         return "{} {} {} {} {}".format(self.__name, self.__title, self.__health, self.__mana, self.__mana_rate)
@@ -50,6 +52,18 @@ class Hero:
     def set_mana_rate(self, mana_rate):
         self.__mana_rate = mana_rate
 
+    def get_weapon(self):
+        return self.__currr_weapon.get_name()
+
+    def set_weapon(self, weapon):
+        self.__curr_weapon = weapon
+
+    def get_spell(self):
+        return self.__cur_spell.name()
+
+    def set_spell(self, spell):
+        self.__curr_spell = spell
+
     def known_as(self):
         return "{} the {}".format(self.__name, self.__title)
 
@@ -86,11 +100,13 @@ class Hero:
         pass
 
     def equip(self, weapon):
-        w = Weapon("axe", 20)
-        return "Equpped with {}, damage: {}".format(w.get_name(), w.get_damage())
+        return "Equipped with {}, damage: {}".format(self.__curr_weapon.name(), self.__curr_weapon.damage())
 
     def learn_spell(self, spell):
-        pass
+        return "Learnt spell {}, damage: {}".format(self.__curr_spell.name(), self.__curr_spell.damage())
 
-    def attack(self, **kwargs):
-        pass
+    def attack(self, by=None):
+        if by == "weapon" and self.__curr_weapon is not None:
+            return self.__curr_weapon.damage()
+        if by == "magic" and self.__curr_spell is not None:
+            return self.__curr_spell.damage()
